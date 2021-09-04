e769d9d511ca6d23f6f0d38404e64cdfcd817465
or-abdillh <hans.abdillh05@gmail.com>
2021-09-04 18:17:09 +0800

    Create Card Component and styling

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="card-wrapper">
+      <img src="/ice-cave.jpeg" />
+      <span class="card-bookmark" >
+         <i class="fa fa-bookmark" ></i>
+      </span>
+      <div class="card-content">
+         <strong>Ice Caves</strong>
+         <small>
+            <i class="fas fa-map-marker-alt"></i>
+            Iceland
+         </small>
+         <small>
+            Starting at
+            <span>$200</span>
+         </small>
+      </div>
+   </div>
+</template>
+
+<script>
+   
+   export default {
+      name: 'Card'
+   }
+   
+</script>
+
+<style lang="scss" >
+   
+   @import './src/sass/_card';
+   
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,17 @@
       <SearchBar></SearchBar>
       <Category></Category>
       <Nav></Nav>
+      <div class="content">
+         <div class="content-title">
+            <strong>Recomended</strong>
+            <small>see more</small>
+         </div>
+         <div class="content-card">
+            <template v-for="card in 4" :key="card" >
+               <Card></Card>
+            </template>
+         </div>
+      </div>
    </section>
 </template>
 
@@ -13,6 +24,7 @@
    import SearchBar from '../components/SearchBar.vue'
    import Category from '../components/Category.vue'
    import Nav from '../components/Nav.vue'
+   import Card from '../components/Card.vue'
    
    export default {
       name: 'Home',
@@ -20,7 +32,8 @@
          Header,
          SearchBar,
          Category,
-         Nav
+         Nav,
+         Card
       }
    }
 </script>
